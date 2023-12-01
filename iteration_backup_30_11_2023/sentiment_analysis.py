8a7f3ff561ff8bb7fc6c9e8f1783747752cd4ad2
Thabang Mposula <78591599+iarxii@users.noreply.github.com>
2023-12-01 09:25:29 +0200

    update analysis script to analyze corrected and spell-checked text, readig csv using row dictionary, debug output to see row being proccessed and row iteration number

--- iteration_backup_30_11_2023/sentiment_analysis.py
+++ iteration_backup_30_11_2023/sentiment_analysis.py
@@ -1,6 +1,7 @@
-# code updated on 30/11/2023 # import nltk
+# code updated on 16/11/2023 # import nltk
 
 import csv
+import sys
 import datetime
 from tabulate import tabulate
 from nltk.sentiment import SentimentIntensityAnalyzer
@@ -8,21 +9,15 @@ from langdetect import detect
 from googletrans import Translator
 from spellchecker import SpellChecker
 import time
-import sys
 
 # Function to read data from CSV file
 def read_csv(file_path, encoding='utf-8'):
     with open(file_path, 'r', encoding=encoding) as csv_file:
-        # csv_reader = csv.reader(csv_file)
+        csv_reader = csv.reader(csv_file)
         # Assuming the first row contains headers
-        # headers = next(csv_reader)
+        headers = next(csv_reader)
         # Read the rest of the data
-        # data = list(csv_reader)
-
-        # copilot suggestion
-        csv_reader = csv.DictReader(csv_file)
-        headers = csv_reader.fieldnames
-        data = [row for row in csv_reader]
+        data = list(csv_reader)
     return headers, data
 
 # Function to perform sentiment analysis
@@ -52,21 +47,9 @@ def detect_and_translate(text):
 # Function to check spelling errors
 def check_spelling(text):
     spell = SpellChecker()
-    
     # Find misspelled words
     misspelled = spell.unknown(text.split())
-    
-    # Correct misspelled words
-    corrected_text = []
-    for word in text.split():
-        if word in misspelled:
-            corrected_text.append(spell.correction(word))
-        else:
-            corrected_text.append(word)
-    
-    # copilot suggestion:
-    corrected_text = [word for word in corrected_text if word is not None]
-    return ' '.join(corrected_text), list(misspelled)
+    return list(misspelled)
 
 # Function to write data to CSV file with timestamp
 def write_to_csv_with_timestamp(data, headers):
@@ -85,30 +68,19 @@ def write_to_csv_with_timestamp(data, headers):
 
     return output_csv_file_path
 
-# Check if command-line arguments are provided
-# if len(sys.argv) != 3:
-#     print("Usage: python your_script.py <csv_file_path> <text_column_name>")
-#     sys.exit(1)
+# Check if a command-line argument is provided
+if len(sys.argv) != 2:
+    print("Usage: python your_script.py <csv_file_path>")
+    sys.exit(1)
 
-# Retrieve command-line arguments
-csv_file_path = sys.argv[1]
-# "GDOHPSA_DPOS_Survey_OP_Data.csv"
-
-text_column_name = sys.argv[2]
-# "patient_comments"
+# Retrieve the CSV file path from the command-line argument
+csv_file_path = "./" + sys.argv[1]
 
 # Record the start time
 start_time = time.time()
 
 # Read data from CSV file with specified encoding
 headers, data = read_csv(csv_file_path, encoding='utf-8')
-
-# Find the index of the column with the specified name
-try:
-    text_column_index = headers.index(text_column_name)
-except ValueError:
-    print(f"Column '{text_column_name}' not found in the CSV file.")
-    sys.exit(1)
 
 # Add new columns for sentiment analysis results, translated text, and misspelled words
 headers.append('Sentiment Score')
@@ -117,33 +89,20 @@ headers.append('Misspelled Words')
 
 # Analyze sentiment, translate, and check spelling for each text in the CSV file
 for row in data:
-    print(row) # debug
-    # print the interation number
-    print(f"Row iteration: {data.index(row)}") # debug
-
-    # If text_column_index is 4, and you're using it to index into the row dictionary, 
-    # that's likely the cause of the error. When you read a CSV file into a dictionary 
-    # using csv.DictReader, the keys in the dictionary are the column names, not the 
-    # column indices. So you should use the column name to access the value:
-    # text = row[text_column_index] # deprecated
-    text = row[text_column_name]
-
-    # Check spelling and correct misspelled words
-    corrected_text, misspelled_words = check_spelling(text)
-
+    text = row[4]  # Assuming the text is in the third column (index 2)
+    
     # Detect language and translate if necessary
-    translated_text = detect_and_translate(corrected_text)
+    translated_text = detect_and_translate(text)
 
     # Analyze sentiment
     sentiment_score = analyze_sentiment(translated_text)
 
-    # row.append(sentiment_score) # deprecated # The error is occurring because you're trying to use the append method on a dictionary.
-    # row.append(translated_text)
-    # row.append(','.join(misspelled_words))
+    # Check spelling
+    misspelled_words = check_spelling(translated_text)
 
-    row['sentiment_score'] = sentiment_score
-    row['translated_text'] = translated_text
-    row['misspelled_words'] = ','.join(misspelled_words)
+    row.append(sentiment_score)
+    row.append(translated_text)
+    row.append(','.join(misspelled_words))
 
 # Record the end time
 end_time = time.time()
@@ -151,12 +110,12 @@ end_time = time.time()
 # Write the table data to the CSV file with timestamp
 output_csv_file_path = write_to_csv_with_timestamp(data, headers)
 
-
-print(f"Table output has been saved to '{output_csv_file_path}'.")
-
 # Calculate and print the duration
 duration = end_time - start_time
 print(f"Processing time: {duration:.2f} seconds.")
+
+# Print a message indicating the output file path
+print(f"Table output has been saved to '{output_csv_file_path}'.")
 
 # user input for indicate end of script execution
 input("Press Enter to exit...")
